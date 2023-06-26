bc00565b0ca75232b25bedd915bbd67e0d070709
Simran Jaiswal <64042895+simran-jaiswal@users.noreply.github.com>
2023-06-27 01:33:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 let btn = document.getElementById('btn');
-let output = document.getElementById('output-text');
-let Number = [Math.floor(Math.random() * 100)]
+let output = document.getElementById('outputtext');
+let number = [Math.floor(Math.random() * 100)]
+btn.addEventListener('click', function() {
+    let input = document.getElementById('userinput').value;
+    if (input == number) {
+        output.innerHTML = `You Guessed right, your no was ${number}`
+
+    } else if (input < number) {
+        output.innerHTML = "You Guessed too low !";
+    };
+    if (input > number) {
+        output.innerHTML = "You Guessed too high";
+    }
+});
